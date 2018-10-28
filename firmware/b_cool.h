c6549660f56f2924165f2bd6de9daa11b9909676
Stenkelfled <github@t-immermann.de>
2018-10-28 10:22:24 +0100

    Add firmware AtmelStudio-Project

---
+++ firmware/b_cool.h
@@ -0,0 +1,17 @@
+#ifndef _B_COOL_H
+#define _B_COOL_H
+
+#define USER_SWITCH_PORT_NAME       A
+#define USER_SWITCH_PORT            PORT_(USER_SWITCH_PORT_NAME)
+#define USER_SWITCH_COUNT           (6)
+
+#define BEER_CAPACITY               (11)
+#define BEER_SWITCH_0_5_PORT_NAME   J
+#define BEER_SWITCH_6_10_PORT_NAME  K
+
+#define WIFI_ENABLE_PORT_NAME       K       
+#define WIFI_ENABLE_PORT            PORT_(WIFI_ENABLE_PORT_NAME)
+#define WIFI_ENABLE_PIN_NR          6
+#define WIFI_ENABLE_PIN             PIN(WIFI_ENABLE_PIN_NR)
+
+#endif
---
+++ firmware/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * B_Cool.cpp
+ *
+ * Created: 28.10.2018 09:50:52
+ * Author : stenkelfled
+ */ 
+
+#include <avr/io.h>
+#include "utils/macro_generation.h"
+#include "b_cool.h"
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
